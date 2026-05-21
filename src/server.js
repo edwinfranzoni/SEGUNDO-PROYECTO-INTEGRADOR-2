b47f3cf044a8e513b170b92c531d8f09887b7e9a
edwin hernandez franzoni <edwin_wii@hotmail.com>
2026-05-20 19:24:21 -0600

    agregar ruta a raiz

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
 
+app.get("/", (req, res) => {
+  res.json({ message: "API funcionando 🚀" });
+});
+
 app.listen (process.env.PORT, () => {  
     console.log("Server up and running");
 
