aa34923cf03d4fe7cc8bc5ea65fa9e046a2223d0
edwin hernandez franzoni <edwin_wii@hotmail.com>
2026-05-21 18:04:37 -0600

    feat: implementar documentacion interactiva con swagger

--- src/app.js
+++ src/app.js
@@ -8,3 +8,14 @@ app.use(router);
 
 export default app;
 
+
+// 1. Importa las librerías usando el formato ESM
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'module';
+
+// Permite cargar archivos JSON en entornos ES Modules
+const requireJson = createRequire(import.meta.url);
+const swaggerDocument = requireJson('../swagger.json'); 
+
+// 2. Agrega la ruta de la documentación (Colócala justo debajo de app.use(router);)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
